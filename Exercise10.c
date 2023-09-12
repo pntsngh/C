88e341cd83bbe8addfcd3d73bed9e4634ad824ce
pntsngh <111980065+pntsngh@users.noreply.github.com>
2023-09-12 13:56:21 +0300

    Add files via upload

---
+++ Exercise10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    while (1) {
+        char string[50];
+        char stop[] = "stop";
+
+        printf("Enter a string:\n");
+        fgets(string, 50, stdin);
+        string[strlen(string) - 1] = '\0';
+        printf("%llu\n", strlen(string));
+
+        int compare = strcmp(stop, string);
+        if (compare == 0) {
+            break;
+        } else {
+            continue;
+        }
+    }
+    return 0;
+}
